dd74747e213646669d892336eafe3c9a6553a1d4
eliduty <eliduty@qq.com>
2023-05-19 09:09:35 +0800

    fix: 增加单元测试

---
+++ tests/sum.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "vitest";
+import { sumFunction } from "@eliduty/test";
+describe("sumFunction", () => {
+  test("adds 1 + 2 to equal 3", () => {
+    expect(sumFunction(1, 2)).toBe(3);
+  });
+});
