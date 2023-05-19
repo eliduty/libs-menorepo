aaa7c2a4ea01fa84e556d077e19f108c7616f18c
eliduty <eliduty@qq.com>
2023-05-19 13:47:35 +0800

    fix: 优化配置

--- .nano-staged.cjs
+++ .nano-staged.cjs
@@ -1,5 +1,5 @@
 module.exports = {
   '*.{html,vue}': ['eslint --fix', 'stylelint --fix', 'prettier --write'],
-  '*.{js,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  '*.{js,ts,tsx,jsx,cjs,mjs}': ['eslint --fix', 'prettier --write'],
   '*.{css}': ['stylelint --fix', 'prettier --write'],
 };
